4a34b04a7b9726346eaeeb6bf8f7eb2e482a7b87
reverie <reverie@draggi.es>
2015-10-07 12:32:05 -0400

    started work on autopartition

--- dothething.sh
+++ dothething.sh
@@ -4,6 +4,7 @@
 HOSTNAME="loki" # only need short form
 LOCALE="en_US.UTF-8" # :911:
 TIMEZONE="America/Detroit" # :911:
+ARCH="$(uname -m)"
 
 init ()
 {
@@ -16,5 +17,43 @@ init ()
         exit 1
     else
         echo "the system is up"
+        # while we're at it do some stuff to make life easier
+        /usr/bin/pacman --noconfirm -Syy && /usr/bin/pacman --noconfirm -S reflector
     fi
+}
+
+partition ()
+{
+    # we assume a few things here
+    # 1) the disk we're installing to is /dev/sda
+    # 2) the disk is at least 500gb in size
+    # 3) the system isn't uefi
+    # 4) that i know how to properly partition a disk
+
+    ###################
+    # 128MB /boot
+    # 1GB swap
+    # rest of disk /
+    ###################
+    # if this doesn't work for you, alter it
+    # heredocs are fun
+    /usr/bin/fdisk /dev/sda <<EOF
+    o
+    n
+    p
+    1
+
+    +128M
+    n
+    p
+    2
+
+    +1G
+    n
+    p
+    3
+
+
+    w
+    EOF
 }
