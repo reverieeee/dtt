5e0b9de5d90a67f39b14ddda67e8c00e3ea315ea
reverie <reverie@draggi.es>
2015-10-07 13:13:27 -0400

    some more code

--- dothething.sh
+++ dothething.sh
@@ -19,7 +19,7 @@ init ()
     else
         echo "the system is up"
         # while we're at it do some stuff to make life easier
-        /usr/bin/pacman --noconfirm -Syy && /usr/bin/pacman --noconfirm -S reflector
+        /usr/bin/pacman --noconfirm -Syy ; /usr/bin/pacman --noconfirm -S reflector
     fi
 }
 
@@ -27,7 +27,7 @@ partition ()
 {
     # we assume a few things here
     # 1) the disk we're installing to is /dev/sda
-    # 2) the disk is at least 500gb in size]]
+    # 2) the disk is at least 500gb in size
     # 3) the system isn't uefi
     # 4) the system has 8gb of RAM
     # 5) that i know how to properly partition a disk
@@ -38,10 +38,26 @@ partition ()
     # rest of disk /
     ###################
     # if this doesn't work for you, alter it
-    # heredocs are fun
     $PARTCMD -a optimal mklabel msdos
     $PARTCMD -a optimal unit mb mkpart primary ext4 1 513
     $PARTCMD set 1 boot on
     $PARTCMD -a optimal unit mb mkpart primary linux-swap 513 8513
     $PARTCMD -a optimal unit mb mkpart primary ext4 8513 100%
+
+    # create and mount filesystems
+    /usr/bin/mkfs.ext4 -q /dev/sda1
+    /usr/bin/mkswap /dev/sda2
+    /usr/bin/swapon /dev/sda2
+    /usr/bin/mkfs.ext4 -q /dev/sda3
+
+    /usr/bin/mount /dev/sda3 /mnt
+    /usr/bin/mkdir -v /mnt/boot
+    /usr/bin/mount /dev/sda1 /mnt/boot
+}
+
+setup ()
+{
+    # mirrorlist setup goes first
+    /usr/bin/cp -v -f /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+    /usr/bin/reflector --verbose --country 'United States' -l 20 -p http --sort rate --save /etc/pacman.d/mirrorlist
 }
